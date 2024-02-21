04243f6aa35ce2122f96199019c5b3c048750cf2
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-02-21 23:18:45 +0300

    Delete .github/scripts directory

--- .github/scripts/update_index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-Latest_Update_File="LATEST_UPDATE"
-Latest_Update=$(cat $Latest_Update_File)
-Massage_Text="**Latest Update**: \`$Latest_Update\`"
-sed -i '18s/.*//' README.md
-sed -i "18i $Massage_Text" README.md
