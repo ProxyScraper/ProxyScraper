d8db32ce9ef9762a2dc704584e3fbf824eae50f4
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-02-21 21:50:12 +0300

    Update update_index.sh

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 Latest_Update_File="LATEST_UPDATE"
 Latest_Update=$(cat $Latest_Update_File)
-Massage_Text="\n**Latest Update** : ` $Latest_Update ` "
-sed -i "/## Usage/i $Massage_Text" README.md
+Massage_Text="**Latest Update**: \`$Latest_Update\`"
+
+
+sed -i "s/\*\*Latest Update\*\*.*$/$Massage_Text/" README.md
+
+
+if ! grep -q "**Latest Update**" README.md; then
+    awk -v var="$Massage_Text" 'NR==2 {print var} 1' README.md > temp.md && mv temp.md README.md
+fi
