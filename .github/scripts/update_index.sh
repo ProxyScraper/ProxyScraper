432ebee7d9cd46aa4451309aac6f6a5218e7fb4e
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-02-21 23:02:16 +0300

    Update update_index.sh

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -3,6 +3,5 @@
 Latest_Update_File="LATEST_UPDATE"
 Latest_Update=$(cat $Latest_Update_File)
 Massage_Text="**Latest Update**: \`$Latest_Update\`"
-
 sed -i '18s/.*//' README.md
 sed -i "18i $Massage_Text" README.md
