3d9bef67fb596781ceb551b8990f848f09a18242
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-02-21 21:36:52 +0300

    Create update_index.sh

---
+++ .github/scripts/update_index.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+Latest_Update_File="LATEST_UPDATE"
+Latest_Update=$(cat $Latest_Update_File)
+Massage_Text="\nLatest Update : $Latest_Update"
+sed -i "/## Usage/i $Massage_Text" README.md
