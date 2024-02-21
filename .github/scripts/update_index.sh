0ca4d6fbf9974c11ea74034288137d12c45a9c5d
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-02-21 22:12:59 +0300

    Update update_index.sh

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 Latest_Update_File="LATEST_UPDATE"
 Latest_Update=$(cat $Latest_Update_File)
+Massage_Text="**Latest Update**: \`$Latest_Update\`"
 
-if [ -n "$Latest_Update" ]; then
-    Massage_Text="**Latest Update**: \`$Latest_Update\`"
-	Massage_Text=$(echo "$Massage_Text" | sed 's/\//\\\//g')
-    usage_line=$(grep -n "## Usage" README.md | cut -d ":" -f 1)
-    sed -i "${usage_line}i $Massage_Text" README.md
+usage_line=$(grep -n "## Usage" README.md | cut -d ":" -f 1)
 
-    if grep -q "**Latest Update**" README.md; then
-        sed -i "/**Latest Update**/d" README.md
-    fi
-else
-    echo "Error: No content found in $Latest_Update_File"
+sed -i "s/\*\*Latest Update\*\*.*$/$Massage_Text/" README.md
+
+
+if ! grep -q "**Latest Update**" README.md; then
+    awk -v var="$Massage_Text" 'NR==2 {print var} 1' README.md > temp.md && mv temp.md README.md
 fi
